bea2bdc0baf025c42b6fc3f2da1fcccd8e5ffa4c
aws1313 <69417093+aws1313@users.noreply.github.com>
2024-01-30 23:27:40 +0100

    pacman ai move nad short train

--- dqn_model.py
+++ dqn_model.py
@@ -1,20 +1,51 @@
 import torch
 import torch.nn as nn
-
+import torch.optim as optim
 
 class DQN(nn.Module):
-    def __init__(self, inp_size: int, hidden_size, out_size: int):
+    def __init__(self, inp_size: int, hidden_size, out_size: int, lr, gamma):
         super(DQN, self).__init__()
         self.rls = nn.Sequential(
+            #nn.Flatten(),
             nn.Linear(inp_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
             nn.Linear(hidden_size, out_size)
         )
-        self.flatten = nn.Flatten()
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss()
+        self.gamma = gamma
+
 
     def forward(self, x):
-        x = self.flatten(x)
         return self.rls(x)
 
     def save(self):
         torch.save(self.state_dict(), './model.pth')
+
+    def train_step(self, old_state, new_state, action, reward):
+        old_state = torch.tensor(old_state, dtype=torch.float)
+        new_state = torch.tensor(new_state, dtype=torch.float)
+        action = torch.tensor(action, dtype = torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+
+        if len(old_state.shape) == 1:
+            old_state = torch.unsqueeze(old_state, 0)
+            new_state = torch.unsqueeze(new_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+
+        prediction = self(old_state)
+        target_prediction = prediction.clone()
+
+        for i in range(len(reward)):
+            Q_new = reward[i]
+            Q_new = reward[i] + self.gamma * torch.max(self(new_state[i]))
+
+            target_prediction[i][torch.argmax(action[i]).item()] = Q_new
+
+        self.optimizer.zero_grad()
+        loss = self.loss(prediction, target_prediction)
+        loss.backward()
+        self.optimizer.step()
--- main.py
+++ main.py
@@ -11,73 +11,120 @@ import sys
 
 sys.path.append(os.path.join(os.getcwd(), c.GAME_DIR))
 print(sys.path)
-
+os.chdir(c.GAME_DIR)
 from game.run import *
-from collections import deque
+from game.pellets import PowerPellet
+from collections import deque, namedtuple
 from dqn_model import DQN
 import torch
+import operator
 from torch import nn
+import math
+from random import sample, random, randint
 import numpy as np
 
-MEM_SIZE = 100
-
+MEM_SIZE = 100000
+BATCH_SIZE = 1000
+LR = 0.001
+GAMMA = 0.9
 DEVICE = ("cuda" if torch.cuda.is_available() else "cpu")
 
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 10000
+
+StateStep = namedtuple("StateStep", ["old_state", "new_state", "action", "reward"])
 
 class PacmanKI:
     def __init__(self):
         self.game = GameController()
         self.mem = deque(maxlen=MEM_SIZE)
-        self.dqn = DQN(2, 2, 5)# .to(DEVICE)
+        self.dqn = DQN(978, 100, 5, LR, GAMMA)  # .to(DEVICE)
+        self.game_count = 0
+        self.epsilon = 0
+        self.steps_done = 0
+        self.last_pallets_eaten = 0
 
     def train_long(self):
         pass
 
-    def train_step(self, old_state):
-        act = self.dqn(torch.tensor(np.array([old_state, old_state], dtype=np.int64), dtype=torch.float))
-        print(act)
-        act = act.argmax().item()
-        print(act)
-        new_state,_,_,_ = self.step(act)
+    def action_step(self, old_state):
+        eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+                        math.exp(-1. * self.steps_done / EPS_DECAY)
+
+        if random() >= eps_threshold:
+            act = torch.argmax(self.dqn(torch.tensor([old_state], dtype=torch.float))).item()
+            print("planned")
+        else:
+            act = randint(0, 4)
+            print("rand")
+
+        self.steps_done += 1
+
+        act = self.set_direction(act)
+        new_state, reward, _, _ = self.step(act)
+        self.cache(old_state, new_state, act, reward)
+        self.dqn.train_step(old_state, new_state, act, reward)
         return new_state
 
-
-
     def step(self, action: int):
-        self.set_direction(action)
         self.game.update()
         return self.eval_state(), self.eval_reward(), False, False
 
     def eval_state(self):
-        print(self.game.pacman.position)
-        return self.vec2_to_tp(self.game.pacman.position)
+        # pellet format => (x, y, super, eaten)
+        for p in self.game.pallets_eaten:
+            p.eaten = True
+        pellets = []
+        pellets.extend(self.game.pellets.pelletList)
+        pellets.extend(self.game.pallets_eaten)
+        pellets.sort(key=operator.attrgetter('id'))
+        ps = []
+        for p in pellets:
+            sup = isinstance(p, PowerPellet)
+            ps.extend([p.position.x, p.position.y, sup, p.eaten])
+
+        s = [
+            self.game.pacman.position.x,
+            self.game.pacman.position.y
+        ]
+        s.extend(ps)
+        return s
 
     def vec2_to_tp(self, vec):
         return [vec.x, vec.y]
 
     def eval_reward(self):
-        return self.game.pellets.numEaten
+        r = self.game.pellets.numEaten - self.last_pallets_eaten
+        self.last_pallets_eaten = self.game.pellets.numEaten
+        print(r)
+        return r
 
-    def cache(self):
-        pass
+    def cache(self, old_state, new_state, action, reward):
+        self.mem.append(StateStep(old_state, new_state, action, reward))
+
+    def sample_cache(self):
+        if len(self.mem) > BATCH_SIZE:
+            return sample(self.mem, BATCH_SIZE)
+        else:
+            return self.mem
+
 
     def set_direction(self, direction: int):
         dirs = [STOP, UP, RIGHT, DOWN, LEFT]
 
         self.game.pacman.want_direction = dirs[direction]
+        return dirs[direction]
 
     def run(self):
         self.game.startGame()
+        self.game.ghosts.ghosts = []
+        self.game.pause.setPause(playerPaused=True)
         self.game.update()
         while True:
-            self.train_step(self.eval_state())
-
-
+            self.action_step(self.eval_state())
 
 
 if __name__ == '__main__':
-    print(f"Running on: {DEVICE}")
-
-    os.chdir(c.GAME_DIR)
     pacman_ki = PacmanKI()
     pacman_ki.run()
