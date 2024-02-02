38298e72e65e331820ad59c0ddcb21a41fdeac15
Maximilian B <43041954+Clafter@users.noreply.github.com>
2024-02-02 09:15:38 +0100

    Verbessere die Lesbarkeit mit Kommentaren

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ LR = 0.002
 GAMMA = 0.9
 DEVICE = ("cuda" if torch.cuda.is_available() else "cpu")
 
+# Definiere die Konstanten der Epsilon Funktion
 EPS_START = 1
 EPS_END = 0.05
 EPS_DECAY = .99999
@@ -40,9 +41,14 @@ StateStep = namedtuple("StateStep", ["old_state", "new_state", "action", "reward
 
 class PacmanKI:
     def __init__(self):
+        # Initialisiere das Pac-Man Spiel
         self.game = GameController()
+
+        # Initialisiere den DQN Algorithmus
         self.mem = deque(maxlen=MEM_SIZE)
         self.dqn = DQN(981, 32, 4, LR, GAMMA, DEVICE).to(DEVICE)
+
+        # Initialisiere verschiedene Variablen für das Training
         self.game_count = 0
         self.epsilon = 0
         self.steps_done = 0
@@ -58,19 +64,24 @@ class PacmanKI:
         self.dqn.train_step(np.array(old_states), np.array(new_states), actions, rewards)
 
     def action_step(self, old_state):
+        # Wähle, ob die Aktion zufällig oder durch das Modell bestimmt wird
+
         global EPS_START
         print("rand prob: "+str(EPS_START))
         if random() <= EPS_START:
+            # Zufällige Aktion
             action = randint(0, 3)
         else:
+            # Aktion durch das Modell bestimmt
             action = torch.argmax(self.dqn(torch.from_numpy(old_state).to(DEVICE))).item()
 
         EPS_START *= EPS_DECAY
         EPS_START = max(EPS_START, EPS_END)
-        self.action_count += 1
 
+        self.action_count += 1
         self.steps_done += 1
 
+        # Führe die Aktion aus und speichere den Zustand
         action = self.set_direction(action)
         new_state, reward, died, won = self.step()
         self.cache(old_state, new_state, action, reward)
@@ -78,6 +89,7 @@ class PacmanKI:
         return died, won
 
     def step(self, ):
+        # Führe einen Schritt im Spiel aus
         self.game.update()
         died = False
         if self.game.pacman_died:
@@ -87,6 +99,7 @@ class PacmanKI:
         return self.eval_state(), self.eval_reward(died, False), died, False
 
     def tile_from_pos(self, x, y):
+        # Konvertiere die Pixelkoordinaten in die Tile-Koordinaten
         x = round((x + 4) / 16)
         y = round(y / 16)
         return x, y
@@ -169,12 +182,7 @@ class PacmanKI:
             return self.mem
 
     def set_direction(self, relative_direction: int):
-        # direction is a int between 0 and 3
-        # 0 = turn left
-        # 1 = turn right
-        # 2 = go straight
-        # 3 = turn around
-        # Convert the relative direction to an absolute direction
+        # Konvertiere die absolute Richtung in eine relative Richtung
 
         current_direction = self.game.pacman.want_direction
         directions = [LEFT, UP, RIGHT, DOWN]
@@ -187,39 +195,39 @@ class PacmanKI:
         if relative_direction < 0 or relative_direction > 3:
             raise ValueError("Invalid relative direction")
 
-        # Calculate the new index based on the relative direction
         new_index = (directions.index(current_direction) + relative_direction) % 4
 
-        # Return the new absolute direction
+        # Setze die neue Richtung im Spiel
         self.game.pacman.want_direction = directions[new_index]
         return directions[new_index]
 
     def run(self):
+        # Starte das Spiel und trainiere das Modell
         self.game.startGame()
         self.game.ghosts.ghosts = [self.game.ghosts.ghosts[0]]
 
         self.game.update()
         self.mode_max = False
-        stuck_steps = 0
-        last_position_x = 0
-        last_position_y = 0
         self.game.pause.setPause(playerPaused=True)
         episode = 0
         while True:
+            # Starte eine neue Episode
             self.last_pac_pos = self.game.pacman.position
             self.last_pallets_eaten = 0
             self.action_count = 0
             episode += 1
-            # Episode
             died, won = False, False
+
+            stuck_steps = 0
+            last_position_x = 0
+            last_position_y = 0
             while not (died or won):
-                # x und y sind die Koordinaten des Tiles auf der Map
-                # Ein tile ist 16x16 Pixel groß und die Map hat ein Offset von 4 Pixeln nach rechts
+                # Führe Schritte aus, bis das Spiel beendet ist
                 x = round((self.game.pacman.position.x + 4) / 16)
                 y = round(self.game.pacman.position.y / 16)
-                died, won = False, False
+
                 if last_position_x != x or last_position_y != y:
-                    # Pacman befindet sich auf einem neuen Tile
+                    # Führe einen Schritt aus, sobald sich die Position geändert hat
                     died, won = self.action_step(self.eval_state())
                     self.stuck = False
 
@@ -228,8 +236,8 @@ class PacmanKI:
                     self.game.update()
 
                     if stuck_steps > 4:
-                        # Pacman hat nach in Richtung des Richtung der Wand bewegt
-                        # Wir müssen die Richtung ändern
+                        # Pacman hat nach in Richtung der Wand bewegt
+                        # -> Er muss die richtung ändern
                         died, won = self.action_step(self.eval_state())
 
                         stuck_steps = 0
@@ -241,6 +249,7 @@ class PacmanKI:
             if episode % 50 == 0:
                 self.train_long()
 
+            # Speichere das Modell alle 100 Episoden ab
             if episode % 100 == 0:
                 os.makedirs("models", exist_ok=True)
                 self.dqn.save(os.path.join("models", "episode_" + str(episode) + ".pth"))
